c53f2efdfdd964bfa413aeca4fbfde7388b15e1a
Aditi-Dharmadhikari <77967495+Aditi-Dharmadhikari@users.noreply.github.com>
2021-02-01 20:05:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+music_peter = "";
+music_Harry = "";
+
+function preload(){
+    music_peter = loadSound("music2.mp3");
+    music_Harry = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600 , 500);
+    
+    video = createCapture();
+    video.hide();  
+}
+
+function draw(){
+    image(video , 0, 0, 600, 500);
+}

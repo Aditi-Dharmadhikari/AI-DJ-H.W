6d38223ebd1a1498683c9a55301d6c00b2bf0630
Aditi-Dharmadhikari <77967495+Aditi-Dharmadhikari@users.noreply.github.com>
2021-02-04 06:42:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 music_peter = "";
 music_Harry = "";
 
+status = "";
+
 rightWristX = 0;
 rightWristY = 0;
 
@@ -10,12 +12,14 @@ leftWristY = 0;
 function preload(){
     music_peter = loadSound("music2.mp3");
     music_Harry = loadSound("music.mp3");
+
+    
 }
 
 function setup(){
     canvas = createCanvas(600 , 500);
-    
-    video = createCapture();
+    canvas.center();
+    video = createCapture(VIDEO);
     video.hide();  
 
     posenet = ml5.poseNet(video , model_load);
@@ -44,4 +48,18 @@ function gotPose(results){
 
 function draw(){
     image(video , 0, 0, 600, 500);
+    stroke("blue");
+    fill("blue");
+    get_status = music_Harry.isPlaying();
+    
+
+    if(scoreleftwrist>0.2){
+        circle(leftWristX, leftWristY, 20);
+        music_peter.stop();
+
+        if(get_status == false){
+        music_Harry.play();
+        document.getElementById("song").innerHTML = "Harry Potter";
+        }
+    }
 }
